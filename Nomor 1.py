95491b3f71d6653bb43de56f96e87f29633ce393
MrShidax <umaralfaris13@gmail.com>
2024-09-01 10:14:50 +0700

    perubahan nomor 1

--- Nomor 1.py
+++ Nomor 1.py
@@ -4,3 +4,5 @@ Akhir = float(input("Enter ending odometer reading=> "))
 Jarak = round(Akhir - Awal,3)
 Harga = round(Jarak * 1.50,3)
 print("You Traveled a distance of",Jarak, "miles. At $1.50 per mile, \nyour fare is $", Harga)
+
+#halo
