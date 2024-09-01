f437752462173c17bc14efb0d90dc6cd551b948d
MrShidax <umaralfaris13@gmail.com>
2024-09-01 11:15:07 +0700

    ubah lagi guys

--- Nomor 13.py
+++ Nomor 13.py
@@ -2,5 +2,4 @@ Jumlah = int(input("Masukkan Jumlah siwa => "))
 Sisa = Jumlah % 30
 Kelas = Jumlah - Sisa
 JumlahKelas = round(Kelas / 30)
-print("Jumlah Kelas yang ada adalah",JumlahKelas,"Kelas")
-print("Siswa tersisa adalah",Sisa,"Siswa")
+print("Jumlah Kelas yang ada adalah",JumlahKelas,"Kelas\nSiswa tersisa adalah",Sisa,"Siswa")
--- Nomor 5.py
+++ Nomor 5.py
@@ -1,5 +1,4 @@
 VTBI = float(input("Volume to be infused (ml) => "))
 Minutes = float(input("Minutes over which to infuse => "))
 Rate = VTBI * 60 / Minutes
-print("VTBI:",VTBI,"ml")
-print("Rate:",Rate,"ml/hr")
+print("VTBI:",VTBI,"ml\nRate:",Rate,"ml/hr")
